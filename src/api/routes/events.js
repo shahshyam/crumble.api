6858a41fb8f45c257f9b9cd04cbdb51c211198e5
shahshyam <shahshyam@users.noreply.github.com>
2020-05-24 20:38:03 +0545

    added updateribbon method

--- src/api/routes/events.js
+++ src/api/routes/events.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const builder = require('xmlbuilder');
 const router = express.Router();
 
 router.post('/api/events', (req, res) => {
@@ -16,6 +17,54 @@ router.post('/api/events', (req, res) => {
             res.status(201);
             return res.json({ message: "The events are created", eventIds: successIds });
         }
+    } else {
+        res.status(500);
+        return res.json({ message: "Unable to process it" });
+    }
+});
+router.get('/api/updateribbon', (req, res) => {
+    let tagId = req.query.tagId    
+    if (tagId) {
+        let doc = builder.create('customUI');
+        doc.att('xmlns', 'http://schemas.microsoft.com/office/2009/07/customui')
+            .ele('ribbon')
+            .ele('tabs')
+            .ele('tab')
+            .att('id', 'test')
+            .att('label', 'HLX Tools')
+            .ele('group')
+            .att('id', 'MyGroup')
+            .att('label', 'Review')
+            .ele('button')
+            .att('id', 'audit')
+            .att('label', 'Audit')
+            .att('size', 'normal')
+            .att('onAction', 'OnClickAction')
+            .up()
+        .ele('button')
+            .att('id', 'impact')
+            .att('label', 'Impact Analysis')
+            .att('size', 'normal')
+            .att('onAction', 'OnClickAction')
+            .up()
+        .ele('button')
+            .att('id', 'comparison')
+            .att('label', 'Compare workbooks')
+            .att('size', 'normal')
+            .att('onAction', 'OnClickAction')
+            .up()
+            .up()
+        .ele('group')
+            .att('id', 'MyGroup2')
+            .att('label', 'About')
+            .ele('button')
+            .att('id', 'about')
+            .att('label', 'About')
+            .att('size', 'large')
+            .att('onAction', 'OnClickAction')
+        
+        res.status(200);
+        return res.json({ message: doc.toString({ pretty: false }) });
     } else {
         res.status(500);
         return res.json({ message: "Unable to process it" });
